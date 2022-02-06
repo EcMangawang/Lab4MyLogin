e7c10b7dfa80b2552621d586b67f4d709cd5daf1
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-02-05 21:19:03 -0700

    Created a User object
    created a user object

---
+++ src/main/java/ca/sait/mylogin/services/AccountService.java
@@ -0,0 +1,13 @@
+package ca.sait.mylogin.services;
+
+/**
+ *
+ * @author emmari
+ */
+public class AccountService {
+    
+    public User login(String username, String password) {
+        
+    }
+    
+}
